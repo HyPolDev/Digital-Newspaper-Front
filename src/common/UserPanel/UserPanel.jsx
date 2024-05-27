0ae5b723036d1f3af7ab93d51e960600bf0627f5
Pol <polmonteroalegre@gmail.com>
2024-05-27 05:22:24 +0200

    FEAT: delete users from admin panel working

--- src/common/UserPanel/UserPanel.jsx
+++ src/common/UserPanel/UserPanel.jsx
@@ -1,9 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import { deleteProfileCall } from "../../services/apiCalls";
 import "./UserPanel.css"
+import { userData } from "../../app/slices/userSlice";
 export const UserPanel = ({ profile }) => {
+
+    const form = document.getElementById("edit")
+
+    const toggleEdit = () => {
+        form.style.visibility == "hidden" ?
+            (form.style.visibility = "visible",
+                form.style.height = "auto") :
+            (form.style.visibility = "hidden",
+                form.style.height = "0");
+        localStorage.setItem("userNameEdited", profile.userName)
+
+    }
+
+    const deleteVisuals = () => {
+        const div = document.getElementById(profile.userName)
+        div.style.color = "red"
+    }
+
+    const rdxUser = useSelector(userData);
+    const dispatch = useDispatch();
+
+    const token = rdxUser.credentials.token
 
     return (
         <>
-            <div className="row-12 centre flex">
+            <div className="row-12 centre flex" id={`${profile.userName}`} style={{ color: "black" }}>
 
                 <div className="col-2">
                     {profile.userName}
@@ -15,6 +40,27 @@ export const UserPanel = ({ profile }) => {
                     {profile.role}
                 </div>
 
+                <div className="col-2 "><div className="dropdown">
+                    <div className="dropbtn flex">
+                        <i className="uil uil-edit"></i>
+                    </div>
+                    <div className="dropdown-content">
+                        <a href="#" onClick={toggleEdit}>Edit Profile</a>
+                        <a href="#" onClick={async () => {
+                            const response = await deleteProfileCall(token, profile.userName)
+                            deleteVisuals()
+                            if (response.success) {
+                                setTimeout(() => {
+                                }, 500)
+                            }
+                            else {
+                                const div = document.getElementById("error")
+                                div.innerHTML = "Cound not delete account please try later"
+                            }
+                        }}>Eliminate Account</a>
+                    </div>
+                </div>
+                </div>
 
             </div>
         </>
--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
-import { getAllUsers } from "../../services/apiCalls"
+import { editProfileCall, getAllUsers } from "../../services/apiCalls"
 import { userData } from "../../app/slices/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { UserPanel } from "../../common/UserPanel/UserPanel";
+import { RoleSelector } from "../../common/RoleSelector/RoleSelector";
 
 export const AdminPanel = () => {
 
@@ -25,14 +26,47 @@ export const AdminPanel = () => {
 
     }, [])
 
+    const [profileEdited, setProfileEditet] = useState({})
+
+    const inputHandler = (e) => {
+        //genero la función que bindea
+        setProfileEditet((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+
+    };
+
+    const submitEdit = async () => {
+        const userName = localStorage.getItem("userNameEdited")
+
+        const response = await editProfileCall(token, userName, profileEdited)
+
+    }
+
     return (
         <>
+            <div className="form centre" id="edit" style={{ position: "relative", height: 0, visibility: "hidden" }}>
+                <input type="text" name="userName" placeholder="User Name" onChange={(e) => inputHandler(e)} />
+                <br />
+                <input type="text" name="realName" placeholder="Real Name" onChange={(e) => inputHandler(e)} />
+                <br />
+                <RoleSelector />
+                <div>Re log in to see the changes</div>
+                <button onClick={submitEdit}>Submit</button>
+                <div id="error"></div>
+
+            </div>
             {Data?.users?.map((item) => {
                 return (
-                    <UserPanel
-                        key={item._id}
-                        profile={item}
-                    ></UserPanel>
+                    <>
+
+                        <UserPanel
+                            key={item._id}
+                            profile={item}
+                        ></UserPanel>
+
+                    </>
                 )
             })}
         </>
